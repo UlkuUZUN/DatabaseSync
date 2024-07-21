86e70066a0fdde1b5367b0e9b0d4796017b4db91
ulku-uzun <105049922+UlkuUZUN@users.noreply.github.com>
2024-07-21 23:42:08 +0300

    Add files via upload

---
+++ DatabaseSync/Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseSync
+{
+    public class CentralDbContext : DbContext
+    {
+        public DbSet<DataItem> DataItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=CentralServer;Initial Catalog=CentralDB;Integrated Security=True");
+        }
+    }
+
+    public class ClientDbContext : DbContext
+    {
+        public DbSet<DataItem> DataItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ClientServer;Initial Catalog=ClientDB;Integrated Security=True");
+        }
+    }
+
+    public class DataItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ DatabaseSync/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace DatabaseSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var centralContext = new CentralDbContext())
+            using (var clientContext = new ClientDbContext())
+            {
+                // Step 1: Get data from central database
+                var centralData = centralContext.DataItems.ToList();
+
+                // Step 2: Get data from client database
+                var clientData = clientContext.DataItems.ToList();
+
+                // Step 3: Determine changes to upload to central
+                foreach (var clientItem in clientData)
+                {
+                    var centralItem = centralData.FirstOrDefault(c => c.Id == clientItem.Id);
+                    if (centralItem == null)
+                    {
+                        // New item in client, add to central
+                        centralContext.DataItems.Add(clientItem);
+                    }
+                    else if (clientItem.LastUpdated > centralItem.LastUpdated)
+                    {
+                        // Updated item in client, update central
+                        centralItem.Name = clientItem.Name;
+                        centralItem.LastUpdated = clientItem.LastUpdated;
+                    }
+                }
+
+                // Step 4: Save changes to central database
+                centralContext.SaveChanges();
+
+                // Step 5: Determine changes to download to client
+                foreach (var centralItem in centralData)
+                {
+                    var clientItem = clientData.FirstOrDefault(c => c.Id == centralItem.Id);
+                    if (clientItem == null)
+                    {
+                        // New item in central, add to client
+                        clientContext.DataItems.Add(centralItem);
+                    }
+                    else if (centralItem.LastUpdated > clientItem.LastUpdated)
+                    {
+                        // Updated item in central, update client
+                        clientItem.Name = centralItem.Name;
+                        clientItem.LastUpdated = centralItem.LastUpdated;
+                    }
+                }
+
+                // Step 6: Save changes to client database
+                clientContext.SaveChanges();
+
+                Console.WriteLine("Synchronization complete.");
+            }
+        }
+    }
+}
